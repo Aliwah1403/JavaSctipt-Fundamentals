7bf4e3699df49ce9b52c93b3e2e7f9b04cb7968a
Curtis Aliwah <aliwahcurtis5@gmail.com>
2023-04-21 20:32:31 +0300

    Started practicing on the review.js file

---
+++ review.js
@@ -0,0 +1,14 @@
+const thisName = "Brian";
+var thatName = "Daniel";
+let fullName = thisName + " " + thatName;
+console.log(fullName);
+
+let lastNameLenght = 0;
+let lastName = "Alphonso"
+lastNameLenght = lastName.length;
+console.log(lastNameLenght);
+
+let name1 = "Odhiambo"
+let letterIndex = " ";
+letterIndex = name1[4];
+console.log(letterIndex);
