ada9a6dad51af8c20de8acb2cb71d97d0daeb674
Curtis Aliwah <aliwahcurtis5@gmail.com>
2023-04-21 21:14:57 +0300

    Added more changes to review.js

--- review.js
+++ review.js
@@ -12,3 +12,19 @@ let name1 = "Odhiambo"
 let letterIndex = " ";
 letterIndex = name1[4];
 console.log(letterIndex);
+let lastLetter = name1[name1.length - 1];
+console.log(lastLetter);
+
+
+const thisArray = [34, 54, 23];
+const sentence = "The largest number is " + thisArray[thisArray.length - 2];
+console.log(sentence);
+
+
+const myArray2 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+const thisData = myArray2[1][2];
+console.log(thisData);
